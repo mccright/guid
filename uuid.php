27406e5aff52d71083f33ce68579d8d3e2ac9a71
mccright <matt.mccright@gmail.com>
2015-12-13 17:44:41 -0600

    Added an option to generate raw uuids
    This version includes a pair of smaller files: uuid.php & uuidclass.php.
    Used to generate raw uuids suitable for use with scripts or applications
    requiring a reasonable-entropy uuid.  For examp, appending a uuid to a
    filename to ensure uniqueness.

---
+++ uuid.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created using Geany.
+ * Author: Matt McCright
+ * Date: 12/13/2015
+ * Uses uuidclass.php, a subset of Guid.php from 
+ * 	Philippe Gaultier -- https://github.com/pgaultier/guid
+ * Generate a Version 5 uuid -- no extra HTML
+ * Suitable for use with scripts or applications requiring a 
+ * 	reasonable-entropy uuid.  For examp, appending a uuid to a filename
+ *  to ensure uniqueness.
+ * GUID references:
+ * https://www.ietf.org/rfc/rfc4122.txt
+ * https://en.wikipedia.org/wiki/Universally_unique_identifier
+ * and
+ * https://en.wikipedia.org/wiki/Globally_unique_identifier
+ */
+/* no  cache headers */
+header("Expires: Mon, 26 Jul 1990 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+require('uuidclass.php');
+
+/* Add some entropy to the namespace */
+function getrand(){
+    $gendguida = array(0,0,0,0,0,0,0,0,0,0,0);
+    for ($i=0;$i<10;$i++) {
+        $n = mt_rand(0, 199999);
+        //print($n);
+        $gendguida[$i] = $n;
+    }
+    //print_r($gendguida);
+    $gendguids = implode($gendguida);
+    return $gendguids;
+    //}
+}
+$v5name = getrand();
+// As a nod to the rfc, add a temporal component
+list($usec, $sec) = explode(' ', microtime());
+$v5name = ($usec . $v5name);
+
+$seedForv5 = (\uuidclass\uuid::v4());
+$guidv5 = \uuidclass\uuid::v5($seedForv5, $v5name);
+print($guidv5);
+
---
+++ uuidclass.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * uuidclass.php
+ *
+ * PHP version 5.3+
+ *
+ * @author    Philippe Gaultier <pgaultier@sweelix.net>
+ * @copyright 2010-2014 Sweelix
+ * @license   http://www.sweelix.net/license license
+ * @version   1.0.0
+ * @link      http://www.sweelix.net
+ * @category  guid
+ * @package   sweelix.guid
+ * @Modified by Matt McCright <matt.mccright@completosec.com>
+ * 
+ */
+
+namespace uuidclass;
+
+/**
+ * Simple guid generator.
+ * A lot of code as been borrowed around the web.
+ *
+ * <code>
+ *  // generate a v4 guid
+ * 	$guid = Guid::v4();
+ * </code>
+ *
+ */
+class uuid {
+	/**
+	 * Generate V4 guid
+	 * @author    Philippe Gaultier <pgaultier@sweelix.net>
+	 * @copyright 2010-2014 Sweelix
+	 * @license   http://www.sweelix.net/license license
+	 * @version   1.0.0
+	 *
+	 * @return string
+	 * @since  1.0.0
+	 */
+	public static function v4() {
+		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+
+				// 32 bits for "time_low"
+				mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+				// 16 bits for "time_mid"
+				mt_rand(0, 0xffff),
+
+				// 16 bits for "time_hi_and_version",
+				// four most significant bits holds version number 4
+				mt_rand(0, 0x0fff) | 0x4000,
+
+				// 16 bits, 8 bits for "clk_seq_hi_res",
+				// 8 bits for "clk_seq_low",
+				// two most significant bits holds zero and one for variant DCE1.1
+				mt_rand(0, 0x3fff) | 0x8000,
+
+				// 48 bits for "node"
+				mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+		);
+	}
+	/**
+	 * Generate V5 guid
+	 * @author    Philippe Gaultier <pgaultier@sweelix.net>
+	 * @copyright 2010-2014 Sweelix
+	 * @license   http://www.sweelix.net/license license
+	 * @version   1.0.0
+	 *
+	 * @param string $namespace guid format : xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+	 * @param string $name      name to encode in guid format
+	 *
+	 * @return boolean|string
+	 * @since  1.0.0
+	 */
+	public static function v5($namespace, $name) {
+		if(!static::isValid($namespace)) return false;
+		$result = false;
+		if(static::isValid($namespace) === true) {
+			// Get hexadecimal components of namespace
+			$nhex = str_replace(array('-','{','}'), '', $namespace);
+
+			// Binary Value
+			$nstr = '';
+
+			// Convert Namespace UUID to bits
+			for($i = 0; $i < strlen($nhex); $i+=2) {
+				$nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+			}
+
+			// Calculate hash value
+			$hash = sha1($nstr . $name);
+			$result = sprintf('%08s-%04s-%04x-%04x-%12s',
+
+					// 32 bits for "time_low"
+					substr($hash, 0, 8),
+
+					// 16 bits for "time_mid"
+					substr($hash, 8, 4),
+
+					// 16 bits for "time_hi_and_version",
+					// four most significant bits holds version number 5
+					(hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+
+					// 16 bits, 8 bits for "clk_seq_hi_res",
+					// 8 bits for "clk_seq_low",
+					// two most significant bits holds zero and one for variant DCE1.1
+					(hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+
+					// 48 bits for "node"
+					substr($hash, 20, 12)
+			);
+		}
+		return $result;
+	}
+
+	/**
+	 * Check if selected guid is valid
+	 *
+	 * @param ustring $uuid guid to check
+	 *
+	 * @return boolean
+	 * @since  1.0.0
+	 */
+	public static function isValid($uuid) {
+		return (preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+				'[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1);
+	}
+}
